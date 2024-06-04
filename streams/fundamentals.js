595c4c02751269b1ac8c8d2b4322fcf5057ddd52
@bruno.moreira <bruno.moreira@syonet.com>
2024-06-04 18:11:31 -0300

    feat(fundamentos-nodejs): criacao de banco de dados fisico/gerenciamento de requisicoes

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,6 +1,6 @@
 // process.stdin.pipe(process.stdout);
 
-import { Readable } from "node:stream";
+import { Readable, Writable, Transform } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -14,10 +14,27 @@ class OneToHundredStream extends Readable {
       } else {
         const buffer = Buffer.from(String(i));
 
-        this.push(`Número: ${buffer}\n`);
+        this.push(buffer);
       }
     }, 1000);
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream());
