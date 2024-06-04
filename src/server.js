03c3db09bb1f6b4d6eb60f9b1fc786db6b797881
Bruno-Moreira-Dev <marbruge@gmail.com>
2024-06-04 15:42:42 -0300

    feat(nodejs/fundamentos): revisao de fundamentos do nodejs

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from "node:http";
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+    });
+
+    return res.writeHead(201).end("Criação de usuários");
+  }
+
+  return res.write(404).end("Not found");
+});
+
+server.listen(3333);
